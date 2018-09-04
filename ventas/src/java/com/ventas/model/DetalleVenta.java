6a313887b898bd87924a252dc24bc78026df56b7
LuisCarlos <ingluiscarlos02@gmail.com>
2018-09-04 15:50:14 -0500

    model

---
+++ ventas/src/java/com/ventas/model/DetalleVenta.java
@@ -0,0 +1,45 @@
+
+package com.ventas.model;
+
+public class DetalleVenta {
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Venta getVenta() {
+        return Venta;
+    }
+
+    public void setVenta(Venta Venta) {
+        this.Venta = Venta;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+    
+    private int codigo;
+    private Venta Venta;
+    private Producto producto;
+    private int cantidad;
+    
+}
---
+++ ventas/src/java/com/ventas/model/Producto.java
@@ -0,0 +1,36 @@
+
+package com.ventas.model;
+
+
+public class Producto {
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+ 
+    
+    
+    private int codigo;
+    private String nombre;
+    private String precio;
+}
