a6e32b04455e8b93a84b95e642b46ed521b903fd
LuisCarlos <ingluiscarlos02@gmail.com>
2018-09-05 10:56:54 -0500

    lista de personas

--- ventas/src/java/com/ventas/Bean/PersonaBean.java
+++ ventas/src/java/com/ventas/Bean/PersonaBean.java
@@ -3,16 +3,14 @@ package com.ventas.Bean;
 
 import com.ventas.dao.PersonaDAO;
 import com.ventas.model.Persona;
-import javax.inject.Named;
-import javax.enterprise.context.RequestScoped;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
-@Named(value = "personaBean")
+
+@ManagedBean
 @RequestScoped
 public class PersonaBean {
-
-   
-    public PersonaBean() {  
-    }
 
     public Persona getPersona() {
         return persona;
@@ -21,6 +19,15 @@ public class PersonaBean {
     public void setPersona(Persona persona) {
         this.persona = persona;
     }
+
+    public List<Persona> getLstPersonas() {
+        return lstPersonas;
+    }
+
+    public void setLstPersonas(List<Persona> lstPersonas) {
+        this.lstPersonas = lstPersonas;
+    }
+    
     
     public void registrar() throws Exception{
         PersonaDAO dao;
@@ -32,5 +39,17 @@ public class PersonaBean {
         }
     }
     
+    
+    public void listar() throws Exception{
+        PersonaDAO dao;
+        try{
+            dao= new PersonaDAO();
+            lstPersonas= dao.listar();
+        }catch(Exception e){
+            throw e;
+        }
+    }
+    
     private Persona persona=new Persona();
+    private List<Persona> lstPersonas;
 }
--- ventas/src/java/com/ventas/dao/PersonaDAO.java
+++ ventas/src/java/com/ventas/dao/PersonaDAO.java
@@ -3,6 +3,9 @@ package com.ventas.dao;
 
 import com.ventas.model.Persona;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PersonaDAO extends DAO {
@@ -12,9 +15,7 @@ public class PersonaDAO extends DAO {
     public void registrar(Persona per) throws Exception{
         try{
             this.Conectar();
-            
-            String sql=("INSERT INTO persona(nombre, sexo) values(?,?)");
-            PreparedStatement st= this.getCn().prepareStatement(sql);
+            PreparedStatement st= this.getCn().prepareStatement("INSERT INTO persona(nombre, sexo) values(?,?)");
             st.setString(1, per.getNombre());
             st.setString(2, per.getSexo());
             st.executeUpdate();
@@ -22,9 +23,33 @@ public class PersonaDAO extends DAO {
             throw e;
         }finally{
             this.Cerrar();
-        }
- 
+        } 
     }
     
     
+    
+    public List<Persona> listar() throws Exception{
+        List<Persona> lista;
+        ResultSet rs;
+        try{
+            this.Conectar();
+            PreparedStatement st=this.getCn().prepareCall("select codigo, nombre, sexo from persona");
+            rs= st.executeQuery();
+            lista=new ArrayList();
+            while(rs.next()){
+                Persona per=new Persona();
+                per.setCodigo(rs.getInt("codigo"));
+                per.setNombre(rs.getString("nombre"));
+                per.setSexo(rs.getString("sexo"));                
+                lista.add(per);                
+            }
+             
+        }catch(Exception e){           
+            throw e;
+        }finally{
+            this.Cerrar();
+        }
+       return lista;
+    }
+   
 }
